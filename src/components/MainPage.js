c752c061f4516d2e2cbe6acef3917b4928bea90c
KevenSoares22 <keven.v.soares@gmail.com>
2023-01-24 21:06:55 -0300

    GuessWord

---
+++ src/components/MainPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './MainPage.css'
+const MainPage = () => {
+  return (
+    <div>
+        
+        
+        
+        <h1>Guess Word</h1>
+        <p>Mostre suas habilidades em advinhar palavras</p>
+        <button className="iniciarBtn">Iniciar</button>
+        
+        </div>
+  )
+}
+
+export default MainPage
